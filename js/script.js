2d4b16f9895858f069ed70f117650adda3de6a5f
alditiya <aldit4@gmail.com>
2023-05-29 17:55:02 +0700

    add feature search and Productc

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-// Toggle class Active
+// Toggle class Active Hamburger
 const navbarNav = document.querySelector('.navbar-nav');
-
 
 // ketika hamburger dibuka
 document.querySelector('#hamburger-menu').
-onclick = () => {
-  navbarNav.classList.toggle('active');
-};
+  onclick = () => {
+    navbarNav.classList.toggle('active');
+  };
 
+// Toggle Class Active Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-button').
+  onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+  };
 
 // function menu
-const hamburgermenu = document.querySelector('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('search-button');
 
-document.addEventListener('click', function (e){
-  if (!hamburgermenu.contains(e.target) && !navbarNav.contains(e.target)) {
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+      searchForm.classList.remove('active');
+    }
   }
 });
